81f94579eb43096b7d73a6cc98451603bc90af46
TnTVlogs <sergi.dalmau0303@gmail.com>
2024-11-19 00:02:35 +0100

    Rebrand

--- index.js
+++ index.js
@@ -229,6 +229,8 @@ function createWindow() {
     win = new BrowserWindow({
         width: 980,
         height: 552,
+        minWidth: 980,
+        minHeight: 552,
         icon: getPlatformIcon('SealCircle'),
         frame: false,
         webPreferences: {
