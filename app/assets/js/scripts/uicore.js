5d8c419ffd605c2082ef06ea44d99e55dbc87c2a
TnTVlogs <sergi.dalmau0303@gmail.com>
2024-12-03 11:37:30 +0100

    updater change

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -49,7 +49,7 @@ if(!isDev){
                 loggerAutoUpdater.info('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/TnTVlogs/MineSy-Client/releases/download/v${info.version}/MineSy-Client-setup-${info.version}${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
+                    info.darwindownload = `https://github.com/TnTVlogs/MineSy-Client/releases/download/v${info.version}/MineSy-Client-setup${process.arch === 'arm64' ? '-arm64' : '-x64'}.dmg`
                     showUpdateUI(info)
                 }
                 
